2f3f8d2bb18beb4eb6d919d7fb24feabc8ac8bbc
Michelle Moon <michellemoon236@gmail.com>
2019-05-22 01:42:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,14 @@
 class Show < ActiveRecord::Base
   has_many :characters 
   has_many :actors, through: :characters
+  
+    # it "can build an associated network" do
+    # # to do this, the show model has to define its relationship with network
+    # show.build_network(:call_letters => "NBC")
+    # expect(show.network.call_letters).to eq("NBC")
+
+  def build_network
+    
+  end
+  
 end
