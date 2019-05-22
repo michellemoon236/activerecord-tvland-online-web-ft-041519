b4244859c68a8a0d24fbe489acd23fff6e5701d8
Michelle Moon <michellemoon236@gmail.com>
2019-05-22 00:52:36 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_genre_to_shows.rb
+++ db/migrate/009_add_genre_to_shows.rb
@@ -1,9 +1,5 @@
 class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
-    create_table :shows do |t|
-      #primary key of :id is created for us!
-      t.string :name
-      t.string :genre
-    end
+    add_column :shows, :genre, :string 
   end
 end
