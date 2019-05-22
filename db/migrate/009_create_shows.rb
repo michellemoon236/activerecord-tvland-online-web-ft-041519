55f8175c275734aa9fce725dd6832839c312bffc
Michelle Moon <michellemoon236@gmail.com>
2019-05-22 00:44:10 +0000

    Automatically backed up by Learn

--- db/migrate/009_create_shows.rb
+++ db/migrate/009_create_shows.rb
@@ -1,6 +1,6 @@
-class CreateActors < ActiveRecord::Migration[4.2]
+class Shows < ActiveRecord::Migration[4.2]
   def change
-    create_table :actors do |t|
+    create_table :shows do |t|
       #primary key of :id is created for us!
       t.string :first_name
       t.string :last_name
