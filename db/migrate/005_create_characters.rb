ded868e8b2cc3bb6602ba6f71101c6aabc3b8f1c
Michelle Moon <michellemoon236@gmail.com>
2019-05-22 01:44:29 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
-      #primary key of :id is created for us!
       t.string :name
       t.integer :show_id
     end
